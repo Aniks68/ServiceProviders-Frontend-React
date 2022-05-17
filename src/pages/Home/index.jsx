a3c24a9b6f8f2ddb66c3b638b5ba417b93d2ce9d
Aniks68 <ikechukwu.anene@decagon.dev>
2022-05-17 19:59:44 +0100

    successfully shufflign menu display

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,9 +4,12 @@ import Footer from '../../components/common/Footer';
 import Banner from '../../components/Home/Banner';
 import Menu from '../../components/common/Menu';
 import { menuItemsData } from '../../components/common/Menu/data';
+import fisherShuffle from '../../components/common/Menu/Shuffler';
 
 const Home = () => {
   const menuRef = useRef();
+
+  const shuffledData = fisherShuffle(menuItemsData).slice(0, 6);
 
   const handleScrollMenu = () => {
     menuRef.current.scrollIntoView({ behavior: 'smooth' });
@@ -15,7 +18,7 @@ const Home = () => {
   return (
     <div>
       <Banner handleScrollMenu={handleScrollMenu} />
-      <Menu list={menuItemsData} ref={menuRef} />
+      <Menu list={shuffledData} ref={menuRef} />
       <Footer />
       <ButtonCartCount />
     </div>
